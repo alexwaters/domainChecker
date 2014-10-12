fa72c214dc756b13ebb7f2792e329291620634ff
alexwaters <alex@coinapex.com>
2014-10-12 04:04:39 -0400

    chunking

--- domains.py
+++ domains.py
@@ -1,30 +1,40 @@
-wordList = "abcdefdeddesxd"
+with open ("domainDict", "r") as theFile:
+    wordList=theFile.read().replace('\n', '')
+
 godaddyBulkLim = 500
+wordSize = 3
+numChunks = (len(wordList) / godaddyBulkLim) + 1
+chunks = []
 
 class Chunk:
     contents = []
     length = len(contents)
     pass
 
-chunks = []
-
-
-def chunkIt():
+def chunkIt(offset):
     tempList = []
     a = 0
     b = 0
+    offset = offset*godaddyBulkLim
     
     while a < len(wordList) and b < godaddyBulkLim:
-        tempList.append(wordList[a:(a+3)])
+        start,fin = (a+offset),(a+offset+wordSize)
+        word = wordList[start:fin]
+        if len(word) == wordSize:
+            tempList.append(word)
         a+=3
         b+=1
+    print "asdas: " + str(tempList)
     return tempList
 
+z=0
+while numChunks > z:
+    chunk = Chunk()
+    chunk.contents = chunkIt(z)
+    chunks.append(chunk.contents)
+    z+=1
 
-tempList = chunkIt()
-
-
-
-print tempList[-1]
-print "List Length: " + str(len(tempList))
 print "Total Length: " + str(len(wordList))
+print "numChunks: " + str(numChunks)
+print "Chunks Length: " + str(len(chunks))
+print str(chunks)
