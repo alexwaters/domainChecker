1b17d2ed51a9ae541b076a28f4a31dfef90741bf
alexwaters <alex@coinapex.com>
2014-10-16 07:25:42 -0400

    A New Hope

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 domainChecker
 =============
+This is a system to help someone find a quality domain name. Fingers crossed,
+Vircurve.com will never appear as a result.
---
+++ archive/domains.py
@@ -0,0 +1,65 @@
+""" Running this file with all functions uncommented will result in several
+files of fixed length. The files contain words which can easily be copied
+and pasted into GoDaddy's bulk domain name checker. This code is deprecated
+and for funny sidelong looks only. These aren't the droids you're looking for.
+"""
+
+with open ("domainDict", "r") as theFile:
+    wordList=theFile.read().replace('\n', '')
+
+listLen = len(wordList)
+gdBulkLim = 230
+wordSize = 3
+words = listLen / wordSize
+numChunks = (words / gdBulkLim) + 1
+chunks = []
+#domExt = ".nyc"
+domExt = ""
+
+class Chunk:
+    offset = 0
+    contents = []
+    pass
+
+def getWords(offset):
+    tempList = []
+    a,b = 0,0
+    off = offset*gdBulkLim*wordSize
+    
+    while a < len(wordList) and b < gdBulkLim:
+        start,fin = (a+off),(a+off+wordSize)
+        
+        word = wordList[start:fin]
+        if len(word) == wordSize:
+            tempList.append(str(word) + domExt)
+        a+=wordSize
+        b+=1
+    print ("Chunk " + str(offset + 1) + " (Len:" + str(len(tempList)) + "): "
+    + str(tempList))
+    return tempList
+
+def chunkIt():
+    counter = 0
+    while numChunks > counter:
+        chunk = Chunk()
+        chunk.offset = counter
+        chunk.contents = getWords(counter)
+        chunks.append(chunk.contents)
+        counter+=1
+    
+def makeFiles():
+    counter = 0
+    while numChunks > counter:
+        with open('chunked'+str(counter)+'.txt', 'w') as the_file:
+            for word in chunks[counter]:
+                the_file.write(str(word)+'\n')
+        counter+=1
+
+def printStats():
+    print "\nTotal Dictionary Length: " + str(listLen)
+    print "Number of words: " + str(words)
+    print "Number of Chunks: " + str(numChunks)
+
+chunkIt()
+#makeFiles()
+printStats()
